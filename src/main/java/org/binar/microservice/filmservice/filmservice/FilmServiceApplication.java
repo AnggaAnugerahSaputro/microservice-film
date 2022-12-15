ec45e73382758601b7f3a973dd0f576e212f7687
AnggaAnugerahSaputro <91450155+AnggaAnugerahSaputro@users.noreply.github.com>
2022-12-16 00:12:47 +0700

    update annotation

--- src/main/java/org/binar/microservice/filmservice/filmservice/FilmServiceApplication.java
+++ src/main/java/org/binar/microservice/filmservice/filmservice/FilmServiceApplication.java
@@ -2,10 +2,12 @@ package org.binar.microservice.filmservice.filmservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FilmServiceApplication {
 
 	public static void main(String[] args) {
